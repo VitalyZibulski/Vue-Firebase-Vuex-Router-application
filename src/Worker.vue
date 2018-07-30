d464bc78d816c016ee11e43a5b0c107e3b353b0e
VitalyZibulski <laravel.vitaly.zibulski2017@gmail.com>
2018-07-30 16:33:42 +0300

    add validation props

--- src/Worker.vue
+++ src/Worker.vue
@@ -1,14 +1,30 @@
 <template>
   <div>
-    <h2>Worker Name: {{ workerName }}</h2>
-    <p>Worker Age: {{ workerAge }}</p>
+    <h2>Worker Name: {{ workerName }} / {{ reverseName }}</h2>
+    <p>Worker Price: {{ workerAge }}</p>
   </div>
 </template>
 
 <script>
-export default {
-    props:['workerName','workerAge']
-}
-
-
+    export default {
+        // props: ['workerName', 'workerPrice'],
+        // props: {
+        //   workerName: String,
+        //   workerAge: Number
+        // },
+        props: {
+            pizzaName: {
+                type: String,
+                // required: true,
+                default: 'Default Worker Name'
+            },
+            pizzaPrice: Number
+        },
+        computed: {
+            reverseName () {
+                return this.pizzaName.split('').reverse().join('')
+            }
+        }
+    }
 </script>
+
