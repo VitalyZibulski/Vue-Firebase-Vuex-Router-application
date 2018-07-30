c8dcf6ad48ddda6596caebba3e31efcee9fa461c
VitalyZibulski <laravel.vitaly.zibulski2017@gmail.com>
2018-07-30 14:34:04 +0300

    add counters

--- src/main.js
+++ src/main.js
@@ -1,7 +1,45 @@
 import Vue from 'vue'
-import App from './App.vue'
+// import App from './App.vue'
+
+Vue.component('my-counter-1', {
+    template: `
+    <div>
+      <h2>Счетчик: {{ counter }}</h2>
+      <button @click="add">+1</button>
+    </div>
+  `,
+    data: function() {
+        return {
+            counter: 0
+        }
+    },
+    methods: {
+        add: function() {
+            this.counter++
+        }
+    }
+})
+
+Vue.component('my-counter-2', {
+    template: `
+    <div>
+      <h2>Счетчик: {{ counter }}</h2>
+      <button @click="add">+1</button>
+    </div>
+  `,
+    data: function() {
+        return {
+            counter: 0
+        }
+    },
+    methods: {
+        add: function() {
+            this.counter++
+        }
+    }
+})
 
 new Vue({
-  el: '#app',
-  render: h => h(App)
+    el: '#app',
+    // render: h => h(App)
 })
