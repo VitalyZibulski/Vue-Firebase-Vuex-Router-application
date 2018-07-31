83f4075dd0eeca5b8337cf66d6f61d92b3ce9a40
VitalyZibulski <laravel.vitaly.zibulski2017@gmail.com>
2018-07-31 13:57:07 +0300

    add navigation menu

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,17 @@
 <template>
   <div class="container">
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+      <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav">
+          <router-link tag="li" class="nav-item" exact to="/" active-class="active">
+            <a class="nav-link">Home</a>
+          </router-link>
+          <router-link tag="li" class="nav-item" to="/workers" active-class="active">
+            <a class="nav-link">Workers</a>
+          </router-link>
+        </ul>
+      </div>
+    </nav>
     <router-view></router-view>
   </div>
 </template>
